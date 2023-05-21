363c612a23105dc0551b8275a480db640a494ff3
devgunhee <devgunhee@gmail.com>
2023-05-21 18:20:59 +0900

    DataBinding Setting

--- app/src/main/java/com/devgunhee/playground/MainActivity.kt
+++ app/src/main/java/com/devgunhee/playground/MainActivity.kt
@@ -2,10 +2,15 @@ package com.devgunhee.playground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.devgunhee.playground.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
